55aa9bf6878edb7c4986c075daee7b201c3af478
Mathijs <mathijs@lermer.nl>
2023-01-16 20:24:04 +0100

    js fix

---
+++ assets/js/mrl.js
@@ -0,0 +1,8 @@
+function hamburgermenuknopje() {
+    var x = document.getElementById('main_menu_01');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
