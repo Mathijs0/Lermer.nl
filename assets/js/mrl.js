d2dcc7ba30d168d240529c64ac0dc9ce72b0bba5
Mathijs <mathijs@lermer.nl>
2023-01-18 08:49:48 +0100

    update

--- assets/js/mrl.js
+++ assets/js/mrl.js
@@ -1,8 +1,8 @@
 function hamburgermenuknopje() {
-    var x = document.getElementById('main_menu_01');
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById('main_menu_01');
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
